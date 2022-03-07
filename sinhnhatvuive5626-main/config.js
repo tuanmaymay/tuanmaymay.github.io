5e47ca19f0430b5ec42dccd782851fe2b56693e2
tuanmaymay <101125518+tuanmaymay@users.noreply.github.com>
2022-03-07 23:29:54 +0700

    Update config.js

--- sinhnhatvuive5626-main/config.js
+++ sinhnhatvuive5626-main/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     introTitle: 'Gửi bạn iuuuu',
-    introDesc: `Nay ngày 8-3 tôi có đôi điều gửi đến bạn!`,
+    introDesc: `Nay ngày 8-3 tôi có đôi điều gửi riêng bạn Ngô!`,
     btnIntro: 'Cảm ơn bạn Tuấn đẹp trai',
     title: 'Chúc bạn một ngày 8/3 thật ý nghĩa, vui tư­ơi ngập tràn hạnh phúc.  😙',
     desc: 'Hôm nay trời đẹp thế nhở, Ỏoooooooooooo. Làm người yêu mình nhé <3',
@@ -8,7 +8,7 @@ const CONFIG = {
     btnNo: 'Đéo :3',
     question:'Tau đùa đấy kk,Có gì muốn nói thì mi cứ ghi vô đây nhé ,có điều bất ngờ đấy =))',
     btnReply: 'Bấm để tiếp tục điều thú zị',
-    reply: 'Chúc bạn nhận đư­ợc nhiều quà, nhiều hoa, nhiều lời khen, lời chúc của phái nam trong ngày hôm nay. Chúc bạn gặp nhiều may mắn hơn, hạnh phúc hơn nhiều niềm vui, nhiều điều tuyệt diệu hơn trong cuộc sống.              ',
+    reply: 'Chúc Ngô 8/3 vui vẻ, xinh đẹp và đậu nhất trường Anh nhé :vv             ',
     mess: '8-3 vui vẻ nhé 🥰. Cảm ơn vì đã xem hết 😘😘',
     messDesc: 'Click nốt cái dưới cho mình vui nhé!!!',
     btnAccept: 'Okiiiii lun <3',
