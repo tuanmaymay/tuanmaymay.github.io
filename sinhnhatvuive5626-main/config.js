24f1e4b6b7f0f54735a3a13ee3d18cf5584cb14f
tuanmaymay <101125518+tuanmaymay@users.noreply.github.com>
2022-03-07 22:47:26 +0700

    Add files via upload

---
+++ sinhnhatvuive5626-main/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Gửi chị iuuuu',
+    introDesc: `Nay ngày lành tháng tốt em có đôi điều gửi đến chị!`,
+    btnIntro: 'Cảm ơn em Hiếu đẹp trai',
+    title: 'Chị iu 17 tuổi, Tuổi 17 ngọt ngào! Thanh xuân vui vẻ nhé 😙',
+    desc: 'Hôm nay trời đẹp thế nhở, Ỏoooooooooooo. Chị cho em tiền nhé <3',
+    btnYes: 'Oke lunnnn',
+    btnNo: 'Ảo tưởng vừa thôi anh trai à :3',
+    question:'Em đùa đấy kk,Có gì muốn nói thì chị  cứ ghi vô đây nhé ,có điều bất ngờ đấy =))',
+    btnReply: 'Bấm để tiếp tục điều thú zị',
+    reply: 'Chị vui vẻ hạnh phúc là được 24/2/2022 ...               ',
+    mess: 'Snvv & Thanh xuân rực rỡ 🥰. Cảm ơn vì đã xem hết 😘😘',
+    messDesc: 'Click nốt cái dưới cho em vui nhé chị!!!',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100054609365791'
+}
